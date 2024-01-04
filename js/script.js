e86010593ef70eb116592d81a867e3dd4a630c53
Miguel <miguelsribeiro7@gmail.com>
2024-01-04 01:43:49 +0000

    finished the base structure for the artist page (carousel)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,41 @@
 const menuBtn = document.querySelector(".hamburguer");
-const mobileMenu = document.querySelector(".mobileNav")
+const mobileMenu = document.querySelector(".mobileNav");
+const carousel = document.querySelectorAll(".photoGallery");
 
-menuBtn.addEventListener("click", () => {
+let isDragStart = false, prevPageX, prevScrollLeft;
+
+for (let i = 0; i < carousel.length; i++) {  // carousel
+    const dragStart = (e) => {
+        // updating global vars on mouse down
+        isDragStart = true;
+        prevPageX = e.pageX || e.touches[0].pageX;
+        prevScrollLeft = carousel[i].scrollLeft;
+    }
+    
+    const dragging = (e) => {
+        // scrolling to left acording to mouse pointer
+        if (!isDragStart) return;
+        e.preventDefault();
+        let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+        carousel[i].scrollLeft = prevScrollLeft - positionDiff;
+    }
+    
+    const dragStop = (e) => {
+        isDragStart = false;
+    }
+    
+    carousel[i].addEventListener("mousedown", dragStart);
+    carousel[i].addEventListener("touchstart", dragStart);
+    
+    carousel[i].addEventListener("mousemove", dragging);
+    carousel[i].addEventListener("touchmove", dragging);
+    
+    carousel[i].addEventListener("mouseup", dragStop);
+    carousel[i].addEventListener("touchend", dragStop);
+}
+
+
+menuBtn.addEventListener("click", () => { //hamburguer menu
     menuBtn.classList.toggle("is-active");
     mobileMenu.classList.toggle("is-active")
 });
